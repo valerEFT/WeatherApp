e825f6a5ab991eccf964ddb9efba4f1f6bf3867f
valera <valeri.kuzmini123@gmail.com>
2025-03-29 12:31:59 +0400

    Removed repeating code

--- src/components/TimeTransformer.ts
+++ src/components/TimeTransformer.ts
@@ -4,17 +4,11 @@ const TimeTransformer = (dt: number, timezone?: number) => {
   const localTimestamp = timezone ? (dt + timezone) * 1000 : dt * 1000;
   const date = new Date(localTimestamp);
 
-  return timezone
-    ? date.toLocaleTimeString("en-GB", {
-        hour: "numeric",
-        minute: "2-digit",
-        hour12: true,
-      })
-    : date.toLocaleTimeString("en-GB", {
-        hour: "numeric",
-        minute: "2-digit",
-        hour12: true,
-      });
+  return date.toLocaleTimeString("en-GB", {
+    hour: "numeric",
+    minute: "2-digit",
+    hour12: true,
+  });
 };
 
 export default TimeTransformer;
